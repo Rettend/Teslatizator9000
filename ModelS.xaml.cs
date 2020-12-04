66c7d5dd7eca2213973cb0b3a686deb452e05ee3
Áron Hegyi <hegyi.aron101@gmail.com>
2020-12-04 15:50:35 +0100

    oldalak

---
+++ ModelS.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Teslatizator9000
+{
+    /// <summary>
+    /// Interaction logic for ModelS.xaml
+    /// </summary>
+    public partial class ModelS : Page
+    {
+        public ModelS()
+        {
+            InitializeComponent();
+        }
+    }
+}
