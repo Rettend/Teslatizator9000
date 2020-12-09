17673794a3d4fbd97676c3e5ccc8b34bb5618499
Redstone12 <75358320+Redstone12@users.noreply.github.com>
2020-12-09 09:43:46 +0100

    konfig kezdés

--- Konfig.xaml.cs
+++ Konfig.xaml.cs
@@ -9,22 +9,21 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Teslatizator9000
 {
     /// <summary>
-    /// Interaction logic for Home.xaml
+    /// Interaction logic for Konfig.xaml
     /// </summary>
-    public partial class Home : UserControl
+    public partial class Konfig : UserControl
     {
-        public Home()
+        public Konfig()
         {
             InitializeComponent();
         }
+
         private void Button_Click_Off(object sender, RoutedEventArgs e)
         {
             App.Current.Shutdown();
@@ -58,20 +57,6 @@ namespace Teslatizator9000
         {
             MainWindow.panel.Children.Clear();
             MainWindow.panel.Children.Add(new Settings());
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Storyboard myStoryboard = (Storyboard)this.Resources["TestStoryboard"];
-            Storyboard.SetTarget(myStoryboard.Children.ElementAt(0) as DoubleAnimation, TeslaLogo);
-            Storyboard.SetTarget(myStoryboard.Children.ElementAt(1) as DoubleAnimation, TeslaLogo);
-            myStoryboard.Begin();
-        }
-        private void Button_Click_Konfig(object sender, RoutedEventArgs e)
-        {
-            MainWindow.panel.Children.Clear();
-            MainWindow.panel.Children.Add(new Konfig());
-
         }
     }
 }
