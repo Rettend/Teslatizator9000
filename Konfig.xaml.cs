071e9a0b08e31b3358fc02db3f15e549e6e7771c
Áron Hegyi <hegyi.aron101@gmail.com>
2020-12-15 19:57:43 +0100

    checkout final

--- Konfig.xaml.cs
+++ Konfig.xaml.cs
@@ -25,7 +25,7 @@ namespace Teslatizator9000
         public string Color { get; private set; }
         public string Tire { get; private set; }
         public string Interior { get; private set; }
-        public List<int> Prices = new List<int>(5) { 37990, 0, 0, 1000, 0 };
+        public List<int> Prices = new List<int>(5) { 37990, 0, 0, 1500, 0 };
         public Konfig()
         {
             InitializeComponent();
@@ -155,7 +155,7 @@ namespace Teslatizator9000
             Model = "Model3";
             DisabledInt.Focusable = false;
             DisabledInt.IsHitTestVisible = false;
-            Prices[0] = 37990;
+            Prices[0] = 46990;
             ResetAll();
             CalcPrice();
         }
@@ -204,9 +204,9 @@ namespace Teslatizator9000
             DefaultInt.IsChecked = true;
             DefaultTire.IsChecked = true;
             DefaultUi.IsChecked = true;
-            Prices[1] = 1500;
+            Prices[1] = 0;
             Prices[2] = 0;
-            Prices[3] = 0;
+            Prices[3] = 1500;
             Prices[4] = 0;
         }
         private void CalcPrice()
@@ -219,6 +219,13 @@ namespace Teslatizator9000
         private void CheckoutClick(object sender, RoutedEventArgs e)
         {
             CheckoutPanel.Visibility = Visibility.Visible;
+            CheckoutTitle.Content = "Autopilot Included";
+            AutopilotButton.Visibility = Visibility.Visible;
+            AutopilotContent.Visibility = Visibility.Visible;
+            CargearBox1.Visibility = Visibility.Hidden;
+            CargearBox2.Visibility = Visibility.Hidden;
+            FinanceInfo1.Visibility = Visibility.Hidden;
+            FinanceInfo2.Visibility = Visibility.Hidden;
         }
         private void CheckoutOff(object sender, RoutedEventArgs e)
         {
@@ -230,14 +237,109 @@ namespace Teslatizator9000
             if (AutopilotButton.IsChecked == true)
             {
                 Prices[4] = 10000;
-                CalcPrice();
             }
             else
             {
                 Prices[4] = 0;
-                CalcPrice();
             }
-            
+            CalcPrice();
+        }
+
+        private void Autopilot_Click(object sender, RoutedEventArgs e)
+        {
+            CheckoutTitle.Content = "Autopilot Included";
+            AutopilotButton.Visibility = Visibility.Visible;
+            AutopilotContent.Visibility = Visibility.Visible;
+            CargearBox1.Visibility = Visibility.Hidden;
+            CargearBox2.Visibility = Visibility.Hidden;
+            FinanceInfo1.Visibility = Visibility.Hidden;
+            FinanceInfo2.Visibility = Visibility.Hidden;
+        }
+        private void Cargear_Click(object sender, RoutedEventArgs e)
+        {
+            CheckoutTitle.Content = "Select Your Car";
+            AutopilotButton.Visibility = Visibility.Hidden;
+            AutopilotContent.Visibility = Visibility.Hidden;
+            CargearBox1.Visibility = Visibility.Visible;
+            CargearBox2.Visibility = Visibility.Visible;
+            FinanceInfo1.Visibility = Visibility.Hidden;
+            FinanceInfo2.Visibility = Visibility.Hidden;
+            if (Model == "ModelS")
+            {
+                PerformancePrice.Content = "$ 91990";
+                LongrangePrice.Content = "$ 69420";
+            }
+            else if (Model == "Model3")
+            {
+                PerformancePrice.Content = "$ 54990";
+                LongrangePrice.Content = "$ 46990";
+            }
+            else if (Model == "ModelX")
+            {
+                PerformancePrice.Content = "$ 99990";
+                LongrangePrice.Content = "$ 79990";
+            }
+            else
+            {
+                PerformancePrice.Content = "$ 59990";
+                LongrangePrice.Content = "$ 49990";
+            }
+        }
+        private void Finance_Click(object sender, RoutedEventArgs e)
+        {
+            CheckoutTitle.Content = "Finance Options";
+            AutopilotButton.Visibility = Visibility.Hidden;
+            AutopilotContent.Visibility = Visibility.Hidden;
+            CargearBox1.Visibility = Visibility.Hidden;
+            CargearBox2.Visibility = Visibility.Hidden;
+            FinanceInfo1.Visibility = Visibility.Visible;
+            FinanceInfo2.Visibility = Visibility.Visible;
+            CarPrice.Content = $"$ {Prices[0]}";
+            ColorPrice.Content = $"$ {Prices[1]}";
+            TirePrice.Content = $"$ {Prices[2]}";
+            IntPrice.Content = $"$ {Prices[3]}";
+            AutopilotPrice.Content = $"$ {Prices[4]}";
+            FinalPrice.Content = $"$ {Prices.Sum()}";
+        }
+        private void Performance_Click(object sender, RoutedEventArgs e)
+        {
+            if (Model == "ModelS")
+            {
+                Prices[0] = 91990;
+            }
+            else if (Model == "Model3")
+            {
+                Prices[0] = 54990;
+            }
+            else if (Model == "ModelX")
+            {
+                Prices[0] = 99990;
+            }
+            else
+            {
+                Prices[0] = 59990;
+            }
+            CalcPrice();
+        }
+        private void Longrange_Click(object sender, RoutedEventArgs e)
+        {
+            if (Model == "ModelS")
+            {
+                Prices[0] = 69420;
+            }
+            else if (Model == "Model3")
+            {
+                Prices[0] = 46990;
+            }
+            else if (Model == "ModelX")
+            {
+                Prices[0] = 79990;
+            }
+            else
+            {
+                Prices[0] = 49990;
+            }
+            CalcPrice();
         }
     }
 }
